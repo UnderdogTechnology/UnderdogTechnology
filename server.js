ecac4faaedce44e650614ca088a14b98ce98c45b
David <divide@ss.underdog>
2016-09-26 19:22:01 +0000

    Added node-express and moved code to client folder

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+var ENV = process.env.NODE_ENV || 'development';
+var clientDir = __dirname + '/client';
+
+app.set('view engine', 'ejs');
+
+var devMode = ENV === 'development';
+
+// setup reload server
+if (devMode) {
+  console.log('DEV: starting livereload');
+  var livereload = require('livereload');
+  var reloadServer = livereload.createServer();
+  reloadServer.watch(clientDir);
+}
+
+app.use(express.static(clientDir));
+
+app.get('/*', function(req, res) {
+  res.render(clientDir + '/index', { dev: devMode });
+});
+
+app.listen(80);
