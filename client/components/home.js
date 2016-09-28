531b7c39f7ac3816f7df958ca4caac77c0d1468b
David <magee.david1994@gmail.com>
2016-09-28 19:16:07 +0000

    added routing animations and loading image

--- client/components/home.js
+++ client/components/home.js
@@ -5,8 +5,7 @@ system.cmp.home = {
     },
     view: function(ctrl, args) {
         return m('div.home', [
-            m('h2', 'Welcome to Underdog Technology!'),
-            m('p', 'The site is currently under construction.')
+            m('h2', 'Welcome to Underdog Technology!')
         ]);
     }
 };
